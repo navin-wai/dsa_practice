fbd769be91367eb35b7a990900abf499b750f852
Navin Kumar <navinkumar677886@gmail.com>
2026-06-22 22:57:06 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        # so this solution is done my bucket sort
+        # so first we make a hash map, where the keys are the numbers 1,2,3...
+        # which represent the frequency of a number , the value of the hash map
+        # is a list which contain all the number that were have that freq,
+
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)  # for here we have 3 = 2times
+        for (n,c) in count.items():  # here c is the count, and n in the number,
+            #n is the key in count and c is the value but in the next line we change it
+            freq[c].append(n)  # now here making it like 2time -> [3, can add more..]
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):#from the last elemnet to 0, by doing -1
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
